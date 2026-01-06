f93b83cde58346dcc42967f80c64b73f645ac637
Jonathan Gao <gsmlg.com@gmail.com>
2026-01-06 21:17:26 +0800

    fix: add dialyzer ignore for admin_conn_case.ex ExUnit functions

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -12,5 +12,6 @@
   # Ignore Mix.shell/0 and ExUnit functions in mix task and test support
   # These are available at runtime but not during dialyzer analysis
   ~r/lib\/mix\/tasks\/test\.e2e\.ex.*unknown_function/,
-  ~r/test\/support\/conn_case\.ex.*unknown_function/
+  ~r/test\/support\/conn_case\.ex.*unknown_function/,
+  ~r/test\/support\/admin_conn_case\.ex.*unknown_function/
 ]
